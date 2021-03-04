148fb1b821537f1b340c4fa73d79d48828b9e73b
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-04 16:29:58 +0900

    Create date setting functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,34 @@
 import {ToDo} from './todo.js';
+
+function render(){
+    // set today's date on the date input field
+    setTodayDate(getTodayDate());
+    saveInfo();
+}
+
+function getTodayDate(){
+    // get today's date and return it as an object
+    const today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth()+1;
+    let day = today.getDate();
+    return {year, month, day};
+}
+
+function padZero(num){
+    // pad one zero before the chosen month or date
+    if(num.toString().length == 1) return '0' + num;
+    else return num;
+}
+
+function setTodayDate(dateObj){
+    // set today's date on the date input field
+    const dateField = document.querySelector('.date-field');
+    let month = padZero(dateObj.month);
+    let day = padZero(dateObj.day);
+    dateField.value = `${dateObj.year}-${month}-${day}`;
+}
+
+render();
+
+// localStorage.clear();
