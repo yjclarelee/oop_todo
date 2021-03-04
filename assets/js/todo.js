ed84974a3c4584d50d3faba59884b191e6a82e68
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-04 21:49:14 +0900

    Changing todo array to list

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,28 +1,27 @@
 export {ToDo};
 
 class ToDo{
-    constructor(todo, completed, year, month, day){
-        this.todo = [todo];
-        this.completed = [completed];
+    constructor(list, year, month, day){
+        this.list = list;
         this.year = year;
         this.month = month;
         this.day = day;
     }
 }
 
-ToDo.prototype.addToDo = function(elem){
-    this.todo.push(elem);
-}
-ToDo.prototype.addCompleted = function(elem){
-    this.todo.push(elem);
-}
+// ToDo.prototype.addToDo = function(elem){
+//     this.todo.push(elem);
+// }
+// ToDo.prototype.addCompleted = function(elem){
+//     this.todo.push(elem);
+// }
 
-ToDo.prototype.deleteToDo = function(elem){
-    let idx = this.todo.indexOf(elem);
-    this.todo.splice(idx, 1);
-}
+// ToDo.prototype.deleteToDo = function(elem){
+//     let idx = this.todo.indexOf(elem);
+//     this.todo.splice(idx, 1);
+// }
 
-ToDo.prototype.deleteCompleted = function(elem){
-    let idx = this.completed.indexOf(elem);
-    this.completed.splice(idx, 1);
-}
+// ToDo.prototype.deleteCompleted = function(elem){
+//     let idx = this.completed.indexOf(elem);
+//     this.completed.splice(idx, 1);
+// }
