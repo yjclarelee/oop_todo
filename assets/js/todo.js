66d1a9cfc9a88b26a2a8b7d80ecf0ec18dd80571
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-03-05 10:12:28 +0900

    Remove unneeded functions

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -8,20 +8,3 @@ class ToDo{
         this.day = day;
     }
 }
-
-// ToDo.prototype.addToDo = function(elem){
-//     this.todo.push(elem);
-// }
-// ToDo.prototype.addCompleted = function(elem){
-//     this.todo.push(elem);
-// }
-
-// ToDo.prototype.deleteToDo = function(elem){
-//     let idx = this.todo.indexOf(elem);
-//     this.todo.splice(idx, 1);
-// }
-
-// ToDo.prototype.deleteCompleted = function(elem){
-//     let idx = this.completed.indexOf(elem);
-//     this.completed.splice(idx, 1);
-// }
